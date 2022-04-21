9e76ab86e0dbd70a9f67586f525363cc4d8cab6b
riyabasak1998 <57060733+riyabasak1998@users.noreply.github.com>
2022-04-21 16:03:57 +0530

    21-4-22 (riya)

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-	<title>LMiit</title>
+	<title>LMiit | Home</title>
 	<!--/tags -->
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
