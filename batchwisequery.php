3154112fcab301dba79d270acb640a4a9675aa44
Subhajit Talukder <subhajit.niharikasoftware@gmail.com>
2022-04-22 20:00:49 +0530

    Update

--- batchwisequery.php
+++ batchwisequery.php
@@ -78,20 +78,20 @@ include('include/menu.php');?>
 						<label for="product" class="control-label">Batch Time<span class="required"></span></label>
 						<select id="time" name="time"  class="form-control" required>
 							<option value="">--Select--</option>
-							<option value="06.30 AM">06.30 AM</option>
-							<option value="07.30 AM">07.30 AM</option>
-							<option value="08.30 AM">08.30 AM</option>
-							<option value="10.30 AM">10.30 AM</option>
-							<option value="11.30 AM">11.30 AM</option>
-							<option value="12.30 PM">12.30 PM</option>
-							<option value="01.30 PM">01.30 PM</option>
-							<option value="02.30 PM">02.30 PM</option>
-							<option value="03.30 PM">03.30 PM</option>
-							<option value="04.30 PM">04.30 PM</option>
-							<option value="05.30 PM">05.30 PM</option>
-							<option value="06.30 PM">06.30 PM</option>
-							<option value="07.30 PM">07.30 PM</option>
-							<option value="08.30 PM">08.30 PM</option>
+							<option value="06.00 AM">06.00 AM</option>
+							<option value="07.00 AM">07.00 AM</option>
+							<option value="08.00 AM">08.00 AM</option>
+							<option value="10.00 AM">10.00 AM</option>
+							<option value="11.00 AM">11.00 AM</option>
+							<option value="12.00 PM">12.00 PM</option>
+							<option value="01.00 PM">01.00 PM</option>
+							<option value="02.00 PM">02.00 PM</option>
+							<option value="03.00 PM">03.00 PM</option>
+							<option value="04.00 PM">04.00 PM</option>
+							<option value="05.00 PM">05.00 PM</option>
+							<option value="06.00 PM">06.00 PM</option>
+							<option value="07.00 PM">07.00 PM</option>
+							<option value="08.00 PM">08.00 PM</option>
 						</select>
 					</div>
 					<div class="form-group">
